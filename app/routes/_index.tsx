34e360c2ddba4bd5b695bd525e4357e0508c24be
Dan Barclay <barclaysd@me.com>
2025-12-21 10:28:47 +0000

    chore: folders

---
+++ app/routes/_index.tsx
@@ -0,0 +1,46 @@
+import { AppSidebar } from '~/components/app-sidebar';
+import { SectionCards } from '~/components/section-cards';
+import { SiteHeader } from '~/components/site-header';
+import { SidebarInset, SidebarProvider } from '~/components/ui/sidebar';
+import type { Route } from './+types/home';
+
+// biome-ignore lint/correctness/noEmptyPattern: react router default
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: 'Promptly' },
+    {
+      name: 'description',
+      content:
+        'The modern CMS for high performing teams to ship effective AI products at speed',
+    },
+  ];
+}
+
+export default function Home() {
+  return (
+    <SidebarProvider
+      style={
+        {
+          '--sidebar-width': 'calc(var(--spacing) * 72)',
+          '--header-height': 'calc(var(--spacing) * 12)',
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader title="Library" />
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              <SectionCards />
+              <div className="px-4 lg:px-6">
+                <div>1</div>
+              </div>
+              <div>2</div>
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
