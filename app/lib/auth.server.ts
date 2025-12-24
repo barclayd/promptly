2302504bb9e0a901a8cbf93eeeb8a2bdf347d58c
Dan Barclay <barclaysd@me.com>
2025-12-24 16:27:41 +0000

    chore: better auth

--- app/lib/auth.server.ts
+++ app/lib/auth.server.ts
@@ -1,5 +1,12 @@
+import { env } from 'cloudflare:workers';
 import { betterAuth } from 'better-auth';
+import { Kysely } from 'kysely';
+import { D1Dialect } from 'kysely-d1';
+
+type Database = Record<string, string>;
 
 export const auth = betterAuth({
-  url: process.env.DATABASE_URL,
+  database: new Kysely<Database>({
+    dialect: new D1Dialect({ database: env.pr }),
+  }),
 });
