38dc55a564ef22b42d2ab034d5358a0c714f5070
Dan Barclay <barclaysd@me.com>
2026-02-21 20:14:34 +0000

    fix: snippet animation

--- app/components/ui/puzzle-pieces.tsx
+++ app/components/ui/puzzle-pieces.tsx
@@ -8,7 +8,7 @@ export const PuzzlePieces = ({ className, ...props }: PuzzlePiecesProps) => {
   return (
     <div className={cn('group relative h-[140px] w-48', className)} {...props}>
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <div className="puzzle-grid scale-[0.59] drop-shadow-lg transition-[rotate] duration-500 ease-in-out group-hover:rotate-90">
+        <div className="puzzle-grid scale-[0.59] transition-[rotate] duration-500 ease-in-out group-hover:rotate-90">
           <div className="puzzle-pc puzzle-tl" />
           <div className="puzzle-pc puzzle-tr" />
           <div className="puzzle-pc puzzle-bl" />
